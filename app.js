61909161160016683aa9dfd7e7a9d1fcd69b9e44
Groutch <hurizen1@hotmail.com>
2018-05-12 17:46:49 +0200

    update files

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 console.log('have fun !');
+$("#first_name").val('');
+$("#last_name").val('');
+$("#city").val('');
 function updateInfo()
 {
     var infos=[$("#first_name").val(),$("#last_name").val(),$("#city").val()];
     console.log(infos);
-    $("#username").text($("#first_name").val()+" "+$("#last_name").val());
-    if($("#first_name").val() == "" && $("#last_name").val() == ""){
-        $("#username").text("Stranger");
+    $("#username").text(infos[0]+" "+infos[1]);
+    if(infos[0] == "" && infos[1] == ""){
+        $("#username").text("stranger");
     }
 }
 $( "input" ).on ("change keyup paste",updateInfo);
